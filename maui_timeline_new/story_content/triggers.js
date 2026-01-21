ebb5f0450fb958add4d3e376f31fab156e464aec
Ebcosio <43482687+Ebcosio@users.noreply.github.com>
2026-01-21 14:16:54 -0600

    uploaded Maui timeline activity

---
+++ maui_timeline_new/story_content/triggers.js
@@ -0,0 +1,200 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5xnTpyC3nu0":
+        Script1();
+        break;
+      case "5jVbPMtp2IE":
+        Script2();
+        break;
+      case "5lUTQQS6idF":
+        Script3();
+        break;
+      case "5VglYGFX8PY":
+        Script4();
+        break;
+      case "5f03756VkQR":
+        Script5();
+        break;
+      case "5gxARATcR20":
+        Script6();
+        break;
+      case "6AvJ5naeZGt":
+        Script7();
+        break;
+      case "6XGpjP0zPEs":
+        Script8();
+        break;
+      case "6mK1JutibUk":
+        Script9();
+        break;
+      case "5VLKyoEtdIX":
+        Script10();
+        break;
+      case "6hbyoLsEewY":
+        Script11();
+        break;
+      case "6Haljj0Vfeu":
+        Script12();
+        break;
+      case "6BVQKyFxBp6":
+        Script13();
+        break;
+      case "69vo1WYFMX1":
+        Script14();
+        break;
+      case "5VxasldKyqB":
+        Script15();
+        break;
+      case "5np3zomHcZe":
+        Script16();
+        break;
+      case "6CwTeczv8M9":
+        Script17();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+window.Script1 = function()
+{
+  const target = object('5udSvvJcYuP');
+const duration = 1000;
+const easing = 'ease-out';
+const id = '6cjEyrKVU0U';
+const pulseAmount = 0.07;
+player.addForTriggers(
+id,
+target.animate(
+[ {scale: '1' }, 
+{scale: `${1 + pulseAmount}` }, 
+{scale: '1' }, 
+{scale: `${1 + pulseAmount}` }, 
+{scale: '1' } ]
+,
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script2 = function()
+{
+  const target = object('5wJWDSoJPao');
+const duration = 1000;
+const easing = 'ease-out';
+const id = '5ZfskrLNLgM';
+const pulseAmount = 0.07;
+player.addForTriggers(
+id,
+target.animate(
+[ {scale: '1' }, 
+{scale: `${1 + pulseAmount}` }, 
+{scale: '1' }, 
+{scale: `${1 + pulseAmount}` }, 
+{scale: '1' } ]
+,
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script3 = function()
+{
+  const target = object('6rPv8YepCEo');
+const duration = 1000;
+const easing = 'ease-out';
+const id = '5ZfskrLNLgM';
+const pulseAmount = 0.07;
+player.addForTriggers(
+id,
+target.animate(
+[ {scale: '1' }, 
+{scale: `${1 + pulseAmount}` }, 
+{scale: '1' }, 
+{scale: `${1 + pulseAmount}` }, 
+{scale: '1' } ]
+,
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script4 = function()
+{
+  const target = object('6CZTWrGsB1l');
+const duration = 1000;
+const easing = 'ease-out';
+const id = '5ZfskrLNLgM';
+const pulseAmount = 0.07;
+player.addForTriggers(
+id,
+target.animate(
+[ {scale: '1' }, 
+{scale: `${1 + pulseAmount}` }, 
+{scale: '1' }, 
+{scale: `${1 + pulseAmount}` }, 
+{scale: '1' } ]
+,
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script5 = function()
+{
+  const target = object('6IDLyrZ5wx8');
+const duration = 1000;
+const easing = 'ease-out';
+const id = '6iMaeHvzypn';
+const pulseAmount = 0.07;
+player.addForTriggers(
+id,
+target.animate(
+[ {scale: '1' }, 
+{scale: `${1 + pulseAmount}` }, 
+{scale: '1' }, 
+{scale: `${1 + pulseAmount}` }, 
+{scale: '1' } ]
+,
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script6 = function()
+{
+  const target = object('6fpIVzY0L2m');
+const duration = 1000;
+const easing = 'ease-out';
+const id = '5fn9b46a50p';
+const pulseAmount = 0.07;
+player.addForTriggers(
+id,
+target.animate(
+[ {scale: '1' }, 
+{scale: `${1 + pulseAmount}` }, 
+{scale: '1' }, 
+{scale: `${1 + pulseAmount}` }, 
+{scale: '1' } ]
+,
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+};
---
+++ maui_timeline_new/story_content/user.js
@@ -0,0 +1,115 @@
+window.InitUserScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+window.Script7 = function()
+{
+  
+
+ document.getElementsByClassName("lightbox-close-btn-floating")[0].style.display = "none";
+
+
+}
+
+window.Script8 = function()
+{
+  
+
+ document.getElementsByClassName("lightbox-close-btn-floating")[0].style.display = "none";
+
+
+}
+
+window.Script9 = function()
+{
+  
+
+ document.getElementsByClassName("lightbox-close-btn-floating")[0].style.display = "none";
+
+
+}
+
+window.Script10 = function()
+{
+  
+
+ document.getElementsByClassName("lightbox-close-btn-floating")[0].style.display = "none";
+
+
+}
+
+window.Script11 = function()
+{
+  
+
+ document.getElementsByClassName("lightbox-close-btn-floating")[0].style.display = "none";
+
+
+}
+
+window.Script12 = function()
+{
+  
+
+ document.getElementsByClassName("lightbox-close-btn-floating")[0].style.display = "none";
+
+
+}
+
+window.Script13 = function()
+{
+  
+
+ document.getElementsByClassName("lightbox-close-btn-floating")[0].style.display = "none";
+
+
+}
+
+window.Script14 = function()
+{
+  
+
+ document.getElementsByClassName("lightbox-close-btn-floating")[0].style.display = "none";
+
+
+}
+
+window.Script15 = function()
+{
+  
+
+ document.getElementsByClassName("lightbox-close-btn-floating")[0].style.display = "none";
+
+
+}
+
+window.Script16 = function()
+{
+  
+
+ document.getElementsByClassName("lightbox-close-btn-floating")[0].style.display = "none";
+
+
+}
+
+window.Script17 = function()
+{
+  
+
+ document.getElementsByClassName("lightbox-close-btn-floating")[0].style.display = "none";
+
+
+}
+
+};
