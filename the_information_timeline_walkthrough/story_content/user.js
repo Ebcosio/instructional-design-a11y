b98a2a6bb6b374dc82a6f3a38f23177062f0f740
Ebcosio <43482687+Ebcosio@users.noreply.github.com>
2026-03-27 10:34:21 -0500

    revised info timeline with variables fixed
    resume button is also hidden. I added this JS code to first slide: try {
    
      localStorage.clear();
    
      sessionStorage.clear();
    
    } catch (e) {}

---
+++ the_information_timeline_walkthrough/story_content/user.js
@@ -0,0 +1,81 @@
+window.InitUserScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+var getKeyDown = player.getKeyDown;
+var keydown = player.keydown;
+var keyup = player.keyup;
+window.Script7 = function()
+{
+  try {
+  localStorage.clear();
+  sessionStorage.clear();
+} catch (e) {}
+}
+
+window.Script8 = function()
+{
+  
+
+ document.getElementsByClassName("lightbox-close-btn-floating")[0].style.display = "none";
+
+
+}
+
+window.Script9 = function()
+{
+  
+
+ document.getElementsByClassName("lightbox-close-btn-floating")[0].style.display = "none";
+
+
+}
+
+window.Script10 = function()
+{
+  
+
+ document.getElementsByClassName("lightbox-close-btn-floating")[0].style.display = "none";
+
+
+}
+
+window.Script11 = function()
+{
+  
+
+ document.getElementsByClassName("lightbox-close-btn-floating")[0].style.display = "none";
+
+
+}
+
+window.Script12 = function()
+{
+  
+
+ document.getElementsByClassName("lightbox-close-btn-floating")[0].style.display = "none";
+
+
+}
+
+window.Script13 = function()
+{
+  
+
+ document.getElementsByClassName("lightbox-close-btn-floating")[0].style.display = "none";
+
+
+}
+
+};
